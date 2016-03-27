02608fee4342da0b0e42576eebb66b1ff07c64d6
steph-harris <steph.harris21@gmail.com>
2016-03-27 18:42:23 -0400

    added two angular routes

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,1 +1,15 @@
-angular.module("portfolio", [])
+angular.module("portfolio", ['ngRoute'])
+  .config(function($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/blog', {
+        templateUrl:"/views/blog.html"
+      })
+      .when('/github', {
+        templateUrl:"views/github.html"
+      });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  });
