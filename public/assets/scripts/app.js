93b347738c7f3ffd21b714ebf6e844e46caee56c
steph-harris <steph.harris21@gmail.com>
2016-03-27 19:05:42 -0400

    made home page

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,6 +1,9 @@
 angular.module("portfolio", ['ngRoute'])
   .config(function($routeProvider, $locationProvider){
     $routeProvider
+      .when('/home', {
+        templateUrl:"/views/home.html"
+      })
       .when('/blog', {
         templateUrl:"/views/blog.html"
       })
