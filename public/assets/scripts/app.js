1f0a0cb28716e7566f4b6420a38d4645b1fdf542
steph-harris <steph.harris21@gmail.com>
2016-03-28 01:15:43 -0400

    created contact html and added related css and angular route

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -4,6 +4,18 @@ angular.module("portfolio", ['ngRoute'])
       .when('/home', {
         templateUrl:"/views/home.html"
       })
+      .when('/about', {
+        templateUrl:"/views/about.html"
+      })
+      .when('/skills', {
+        templateUrl:"/views/skills.html"
+      })
+      .when('/showcase', {
+        templateUrl:"/views/showcase.html"
+      })
+      .when('/contact', {
+        templateUrl:"/views/contact.html"
+      })
       .when('/blog', {
         templateUrl:"/views/blog.html"
       })
