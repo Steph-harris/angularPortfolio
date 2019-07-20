b882dc3f50fe7d3daa41aa42c2368df98357b9b5
steph-harris <steph.harris21@gmail.com>
2019-07-20 12:37:08 -0400

    fixing Github commit fetch

--- public/assets/scripts/controllers/github.js
+++ public/assets/scripts/controllers/github.js
@@ -6,10 +6,11 @@ angular.module("portfolio")
         $scope.repos = response.data;
       });
     //when a repo is clicked, get my commits, sha, and date
-    $scope.getCommits = function(){
-      $http.get('https://api.github.com/repos/Steph-harris/' + $scope.repo.name + '/commits')
+    $scope.getCommits = function(repoName){
+      $http.get('https://api.github.com/repos/Steph-harris/' + repoName + '/commits?per_page=10')
         .then(function(commits){
-          $scope.repos.commits = commits.data;
+          console.log(commits);
+          $scope.commits = commits.data;
         });
     }
     //clicking sha takes user to that commits github page
